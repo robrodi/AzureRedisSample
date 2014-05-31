412775dc81dd5e2adc12c4de576fde2399987ba8
Rob Rodi <robert.rodi@gmail.com>
2014-05-31 15:17:46 -0700

    connection achieved

--- AzureRedisSample/AzureRedisSample.Tests/SimpleTests.cs
+++ AzureRedisSample/AzureRedisSample.Tests/SimpleTests.cs
@@ -1,11 +1,21 @@
-﻿using Xunit;
+﻿using System.IO;
+using System.Reflection;
+using Xunit;
 namespace AzureRedisSample.Tests
 {
     public class SimpleTests
     {
+        private string configuration;
+
         [Fact]
         public void First()
         {
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("AzureRedisSample.Tests.RedisKey"))
+            using (var s = new StreamReader(stream))
+            {
+                configuration = s.ReadToEnd();
+            }
+            var connection = StackExchange.Redis.ConnectionMultiplexer.Connect(configuration);
         }
     }
 }
