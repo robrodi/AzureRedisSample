38213bfc7ad72311e0fd77bd5ec2ee8c9b2910b4
Rob Rodi <robert.rodi@gmail.com>
2014-05-31 09:21:03 -0700

    basics added

---
+++ AzureRedisSample/AzureRedisSample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureRedisSample.Startup))]
+namespace AzureRedisSample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
