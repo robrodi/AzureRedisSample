451fd1deab0a890c4ad46887db4d3fa0cc330610
Rob Rodi <robert.rodi@gmail.com>
2014-06-01 08:05:30 -0700

    timed test

--- AzureRedisSample/AzureRedisSample.Tests/SimpleTests.cs
+++ AzureRedisSample/AzureRedisSample.Tests/SimpleTests.cs
@@ -1,33 +1,71 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Threading.Tasks;
+using NLog;
 using StackExchange.Redis;
 using Xunit;
 namespace AzureRedisSample.Tests
 {
     public class SimpleTests
     {
-        Lazy<ConnectionMultiplexer> connection = new Lazy<ConnectionMultiplexer>(Connect);
+        Logger log = NLog.LogManager.GetCurrentClassLogger();
+
+        readonly Lazy<IDatabase> connection = new Lazy<IDatabase>(Connect);
+
+        private IDatabase Connection
+        {
+            get
+            {
+                return connection.Value;
+            }
+        }
+
         [Fact]
         public void First()
         {
+            log.Info("==============Starting Test First==============");
             Assert.NotNull(connection.Value);
         }
 
         [Fact]
-        public void SortedList()
+        public void SmallSortedList()
         {
             RedisKey key = "SortedList.Key";
-            var db = connection.Value.GetDatabase();
-            db.KeyDelete(key);
-            db.SortedSetAdd(key, "First", 1D);
-            db.SortedSetAdd(key, "Fourth", 2D);
-            db.SortedSetAdd(key, "Third", 1.2D);
-            db.SortedSetAdd(key, "Second", 1.1D);
-            var result = db.SortedSetScan(key);
+
+            log.Info("==============Starting Test SmallSortedList==============");
+            var s = Stopwatch.StartNew();
+            log.Trace("Purging DB {0}", s.ElapsedMilliseconds);
+            Connection.KeyDelete(key);
+            log.Trace("Purging DB -> Done {0}", s.ElapsedMilliseconds);
+
+            s.Restart();
+
+            Tuple<string, double>[] values =
+            {
+                new Tuple<string, double>("First", 1D),
+                new Tuple<string, double>("Fourth", 2D),
+                new Tuple<string, double>("Third", 1.2D),
+                new Tuple<string, double>("Second", 1.1D)
+            };
+
+            log.Trace("Inserting Keys {0}", s.ElapsedMilliseconds);
+            var promises = values.Select(value => Connection.SortedSetAddAsync(key, value.Item1, value.Item2)).Select(t => (Task) t);
+
+            Task.WaitAll(promises.ToArray());
+            log.Trace("Inserting Keys -> Done {0}", s.ElapsedMilliseconds);
+
+            log.Trace("Getting Keys {0}", s.ElapsedMilliseconds);
+            var result = Connection.SortedSetScan(key);
             Assert.NotNull(result);
+            log.Trace("Getting Keys -> Have Enumerator {0}", s.ElapsedMilliseconds);
+
             var resultArray = result.ToArray();
+            log.Trace("Getting Keys -> Done {0}", s.ElapsedMilliseconds);
+
             Assert.Equal(4, resultArray.Length);
             Assert.Equal((RedisValue) "First", resultArray[0].Element);
             Assert.Equal((RedisValue)"Second", resultArray[1].Element);
@@ -35,13 +73,12 @@ namespace AzureRedisSample.Tests
             Assert.Equal((RedisValue)"Fourth", resultArray[3].Element);
         }
 
-
-        private static ConnectionMultiplexer Connect()
+        private static IDatabase Connect()
         {
             using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("AzureRedisSample.Tests.RedisKey"))
             using (var s = new StreamReader(stream))
             {
-                return ConnectionMultiplexer.Connect(s.ReadToEnd());
+                return ConnectionMultiplexer.Connect(s.ReadToEnd()).GetDatabase();
             }
         }
     }
