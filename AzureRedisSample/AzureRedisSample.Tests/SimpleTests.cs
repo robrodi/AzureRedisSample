6454bb9782b3c3c67772405a1329194084f5490a
Rob Rodi <robert.rodi@gmail.com>
2014-06-01 07:47:35 -0700

    simple sorted list

--- AzureRedisSample/AzureRedisSample.Tests/SimpleTests.cs
+++ AzureRedisSample/AzureRedisSample.Tests/SimpleTests.cs
@@ -1,21 +1,49 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Linq;
 using System.Reflection;
+using StackExchange.Redis;
 using Xunit;
 namespace AzureRedisSample.Tests
 {
     public class SimpleTests
     {
-        private string configuration;
-
+        Lazy<ConnectionMultiplexer> connection = new Lazy<ConnectionMultiplexer>(Connect);
         [Fact]
         public void First()
+        {
+            Assert.NotNull(connection.Value);
+        }
+
+        [Fact]
+        public void SortedList()
+        {
+            RedisKey key = "SortedList.Key";
+            var db = connection.Value.GetDatabase();
+            db.KeyDelete(key);
+            db.SortedSetAdd(key, "First", 1D);
+            db.SortedSetAdd(key, "Fourth", 2D);
+            db.SortedSetAdd(key, "Third", 1.2D);
+            db.SortedSetAdd(key, "Second", 1.1D);
+            var result = db.SortedSetScan(key);
+            Assert.NotNull(result);
+            var resultArray = result.ToArray();
+            Assert.Equal(4, resultArray.Length);
+            Assert.Equal((RedisValue) "First", resultArray[0].Element);
+            Assert.Equal((RedisValue)"Second", resultArray[1].Element);
+            Assert.Equal((RedisValue)"Third", resultArray[2].Element);
+            Assert.Equal((RedisValue)"Fourth", resultArray[3].Element);
+        }
+
+
+        private static ConnectionMultiplexer Connect()
         {
             using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("AzureRedisSample.Tests.RedisKey"))
             using (var s = new StreamReader(stream))
             {
-                configuration = s.ReadToEnd();
+                return ConnectionMultiplexer.Connect(s.ReadToEnd());
             }
-            var connection = StackExchange.Redis.ConnectionMultiplexer.Connect(configuration);
         }
     }
 }
+;
